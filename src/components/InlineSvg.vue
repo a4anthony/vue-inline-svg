b17304393d51b1b9e625f0cafe61e5ca895d9458
Anthony Akro <anthonygakro@gmail.com>
2021-10-15 07:49:54 +0100

    Update InlineSvg.vue

--- src/components/InlineSvg.vue
+++ src/components/InlineSvg.vue
@@ -1,62 +1,70 @@
 <script>
 class Svg {
   constructor(path) {
-    const div = document.createElement("div");
-    div.innerHTML = path;
+    const div = document.createElement('div')
+    div.innerHTML = path
     // change to wherever your svg files are
-    const fragment = document.createDocumentFragment();
-    fragment.appendChild(div);
-    this.svg = fragment.querySelector("svg");
+    const fragment = document.createDocumentFragment()
+    fragment.appendChild(div)
+    this.svg = fragment.querySelector('svg')
   }
   classes(classes) {
     if (classes) {
-      this.svg.classList = "";
-      classes.split(" ").forEach(className => {
-        this.svg.classList.add(className);
-      });
+      this.svg.classList = ''
+      classes.split(' ').forEach((className) => {
+        this.svg.classList.add(className)
+      })
     }
-    return this;
+    return this
   }
   width(width) {
     if (width) {
-      this.svg.setAttribute("width", width);
+      this.svg.setAttribute('width', width)
     }
-    return this;
+    return this
   }
 
   height(height) {
     if (height) {
-      this.svg.setAttribute("height", height);
+      this.svg.setAttribute('height', height)
     }
-    return this;
+    return this
   }
   toString() {
-    return this.svg.outerHTML;
+    return this.svg.outerHTML
   }
 }
 
-import { h } from "vue";
+import { h } from 'vue'
 
 export default {
   props: {
     path: { type: String, required: true },
     classes: {
       type: String,
-      default: ""
+      default: '',
     },
     size: {
       type: Number,
-      default: 15
-    }
+      default: 15,
+    },
+    width: {
+      type: Number,
+      default: this.size,
+    },
+    height: {
+      type: Number,
+      default: this.size,
+    },
   },
   render() {
-    return h("div", {
-      classList: "flex-items-center",
+    return h('div', {
+      classList: 'flex-items-center',
       innerHTML: `<span>${new Svg(this.path)
         .classes(this.classes)
-        .width(this.size)
-        .height(this.size)}</span>`
-    });
-  }
-};
+        .width(this.width)
+        .height(this.height)}</span>`,
+    })
+  },
+}
 </script>
